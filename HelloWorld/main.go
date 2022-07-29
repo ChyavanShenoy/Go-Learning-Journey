30fe40077b84757663c7dfff38370dd0128c3527
Chyavan Shenoy <32121448+ChyavanShenoy@users.noreply.github.com>
2022-07-30 04:16:06 +0530

    Learnings from the program

--- HelloWorld/main.go
+++ HelloWorld/main.go
@@ -5,3 +5,9 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+// Main allows you to compile an executable program.
+// should also contain a function called main.
+
+// To run a Go program, type go run main.go.
+// To compile a Go program for installation, type go build main.go.
